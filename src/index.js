f6a0ae583b9b6127a831771875da5a1de2762a79
Edouard de Laage <edouarddelaage@gmail.com>
2018-09-21 17:54:03 +0200

    Using Redx - Basic Redux flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import { createStore } from "redux"
+
+// Apprentissage sur la base du site : https://www.youtube.com/watch?v=ZKCYqJu4n3s&list=PL55RiY5tL51rrC3sh8qLiYHqUV3twEYU_&index=3
+
+
+//Une fois l’action “ dispatchée “, nous avons besoin de fonctions qui écoutent les actions et modifient le state en conséquence.
+// Ce sont les reducers.
+// Chaque reducer prend en charge une partie du state. Un reducer a pour but de décrire le state initial et de retourner le nouveau state pour les différents types d’actions qu’il prend en charge :
+// https://gkueny.fr/react-lecon-4
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBSTRACT":
+      state = state - action.payload;
+      break;
+    default:
+      return state;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 1)
+
+store.subscribe(() => {
+  console.log("Store updated : ", store.getState())
+})
+
+store.dispatch({
+  type: "ADD",
+  payload: 100
+})
+
+store.dispatch({
+  type: "ADD",
+  payload: 22
+})
+
+store.dispatch({
+  type: "SUBSTRACT",
+  payload: 80
+})
